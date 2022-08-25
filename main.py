697f3222ab8cb5fec7c925bbb9aba2cfbe8975b3
mgh12453 <mgh12453@gmail.com>
2022-08-25 16:56:46 +0430

    Add file_path echo Handler

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def fileid(update, context):
     with open(filename, 'wb') as f:
         context.bot.get_file(update.message.document).download(out=f)
     
-    context.bot.sendPhoto(chat_id=chatid, caption=filename, photo=open(filename, 'rb').read())
+    context.bot.sendPhoto(chat_id=chatid, caption=filename.rsplit('.', 1)[0], photo=open(filename, 'rb').read())
     os.remove(filename)
     # update.message.reply_photo(update.message.document, caption=filename)
     # update.message.reply_text(update.message.document.file_name)
