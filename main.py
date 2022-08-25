a9879f3a7324f36c98370d1b6923160f924def90
mgh12453 <mgh12453@gmail.com>
2022-08-25 14:17:40 +0430

    Add file_path echo Handler

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def echo(update, context):
     update.message.reply_text(update.message.text)
 
 def fileid(update, context):
-    update.message.reply_text(update.message.document.file_id)
+    # update.message.reply_text(update.message.document.file_id)
     update.message.reply_text(update.message.document.file_path)
 
 def error(update, context):
